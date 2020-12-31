a15e4c4cfa2fb5a070eae5797c59eb48d5b0f86f
unknown <a01088807068@gmail.com>
2020-12-31 09:28:25 +0900

    upload files

--- d6/src/d6/PigSaveTest.java
+++ d6/src/d6/PigSaveTest.java
@@ -2,7 +2,6 @@ package d6;
 
 public class PigSaveTest {
 public static void main(String[] args) {
-	PigSave save = new PigSave();
-	System.out.println(save);
+
 }
 }
---
+++ d7/src/d7/PiggyService.java
@@ -0,0 +1,26 @@
+package d7;
+
+
+public class PiggyService {
+
+	int balance = 0;
+
+	// 입금
+	public void deposit(int money) {
+		balance += money;
+	}
+
+	// 출금
+	public String withdraw(int money1) {
+
+		String str = "";
+
+		if (balance - money1 < 0) {
+			str = "잔액이 모자랍니다. 다시 입력주세요.";
+		} else {
+			balance -= money1;
+		}
+		return str;
+		//sysout을 사용하지 못하기 때문에 리턴값을 스트링으로 변경하였다.
+	}
+}
