e4f55eeaf633415eec1faff26327fcdf01b6986d
unknown <a01088807068@gmail.com>
2021-01-04 09:24:22 +0900

    upload files

--- bmi/src/org/bmi/ui/BMIUserInterface.java
+++ bmi/src/org/bmi/ui/BMIUserInterface.java
@@ -35,40 +35,21 @@ public class BMIUserInterface {
 		double weight = Double.parseDouble(this.inputStr("본인의 몸무게"));
 
 		System.out.println(name + "님의 키는 : " + height + " 몸무게는 : " + weight);
-//		double bmi = service.calculate(height, weight);
-//		System.out.println(bmi);
-//		String bmiStr = "";
-//		if (bmi < 18.5) {
-//			bmiStr = "저체중";
-//		} else if (bmi <= 23) {
-//			bmiStr = "정상";
-//		} else if (bmi <= 25) {
-//			bmiStr = "경도비만";
-//		} else if (bmi <= 30) {
-//			bmiStr = "고도비만";
-//		} else if (30 < bmi) {
-//			bmiStr = "초고도비만";
-//		}
-//		System.out.println("당신은" + bmiStr + "입니다.");
+		double bmi = service.calculate(height, weight);
+		System.out.println(bmi);
+		String bmiStr = "";
+		if (bmi < 18.5) {
+			bmiStr = "저체중";
+		} else if (bmi <= 23) {
+			bmiStr = "정상";
+		} else if (bmi <= 25) {
+			bmiStr = "경도비만";
+		} else if (bmi <= 30) {
+			bmiStr = "고도비만";
+		} else if (30 < bmi) {
+			bmiStr = "초고도비만";
+		}System.out.println("당신은"+bmiStr+"입니다.");
 	}
 
-//	public void output() {
-//
-//		double bmi = service.calculate(height, weight);
-//		System.out.println(bmi);
-//		String bmiStr = "";
-//		if (bmi < 18.5) {
-//			bmiStr = "저체중";
-//		} else if (bmi <= 23) {
-//			bmiStr = "정상";
-//		} else if (bmi <= 25) {
-//			bmiStr = "경도비만";
-//		} else if (bmi <= 30) {
-//			bmiStr = "고도비만";
-//		} else if (30 < bmi) {
-//			bmiStr = "초고도비만";
-//		}
-//		System.out.println("당신은" + bmiStr + "입니다.");
-//	}
 
 }
